8a40b06adebed4d982dda47f06c280e119092ddb
mattis03 <mattias.neidenstrom@bahnhof.se>
2024-12-14 15:05:22 +0100

    Lade till fler hjälpmetoder

--- 2024/InputParser.java
+++ 2024/InputParser.java
@@ -1,16 +1,20 @@
 import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
 public class InputParser {
-    private final LinkedList<String> LINES = new LinkedList<>();
+    private final ArrayList<String> LINES = new ArrayList<>();
+    private String singleString = "";
 
     public InputParser(String fileName) {
         try {
             Scanner sc = new Scanner(new File(fileName));
-            while (sc.hasNext())
-                LINES.add(sc.nextLine());
+            while (sc.hasNext()) {
+                String nextLine = sc.nextLine();
+                LINES.add(nextLine);
+                singleString += nextLine + "\n";
+            }
             sc.close();
         } catch (Exception e) {
             System.out.println("File read error");
@@ -24,6 +28,14 @@ public class InputParser {
 
     public String getLine(int lineIndex) {
         return LINES.get(lineIndex);
+    }
+
+    public char getChar(int x, int y) {
+        return LINES.get(y).charAt(x);
+    }
+
+    public String singleString() {
+        return singleString;
     }
 
     public Iterable<String> lines() {

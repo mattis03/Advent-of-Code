ac5c13aa53dd3f62aba0c2d0fb8c21f1db7d0f4b
mattis03 <mattias.neidenstrom@bahnhof.se>
2025-11-30 17:29:42 +0100

    Renamed folder "Day1" -> "Day01" and removed AoC 2024 test

---
+++ 2025/Day01/Part2.cpp
@@ -0,0 +1,11 @@
+#include "../InputParser.cpp"
+#include "../StringReader.cpp"
+#include "../StopWatch.cpp"
+#include "../ProgressBar.cpp"
+
+int main(void)
+{
+    InputParser ip("Day1\\input.txt");
+    
+    return 0;
+}
